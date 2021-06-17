3743246673f6e1d5f0eb36081fafbded38101ec2
Vania Porto <vaniaportho@gmail.com>
2021-06-17 15:15:37 -0300

    Criou o menu da Calculadora no metodo main, a chamada da função somarDoisNumeros e o seu respectivo teste unitario

---
+++ src/test/java/unitTests/TestaCalc.java
@@ -0,0 +1,23 @@
+package unitTests;
+
+import devcalc.Calc;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestaCalc {
+    @Test
+    public void testarSomarDoisNumeros() {
+        // 1 - Prepara - Configurar - Given
+        int num1 = 5;
+        int num2 = 7;
+        int resultadoEsperado = 12;
+
+        // 2 - Executar - When
+        int resultadoAtual = Calc.somarDoisNumeros(num1, num2);
+
+        // 3 - Validar - Then
+        assertEquals(resultadoEsperado,resultadoAtual);
+    }
+
+}
