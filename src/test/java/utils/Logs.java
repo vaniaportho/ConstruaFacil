0c22f9c1319673ebb77a4d9792d652475bd49a18
Vania Porto <vaniaportho@gmail.com>
2021-07-19 17:50:00 -0300

    Cadastro, consulta e login de um usuário

---
+++ src/test/java/utils/Logs.java
@@ -0,0 +1,64 @@
+package utils;
+
+import com.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Logs {
+
+    String dataHora;
+
+    // Criar o arquivo de log no inicio da execução
+    public void iniciarLogCSV(String dataHora) throws IOException {
+        String [] cabecalho = {"data e hora", "caso de teste", "mensagem"};
+        this.dataHora = dataHora;
+        Writer escritor = Files.newBufferedWriter(Paths.get("target/logs/log - " + dataHora + ".csv"));
+        CSVWriter escritorCSV = new CSVWriter(escritor);
+
+        escritorCSV.writeNext(cabecalho);
+
+        escritorCSV.flush();
+        escritor.close();
+    }
+
+    // Gravar cada linha no log
+    public void registrarCSV(String dataHoraLog, String casoDeTeste, String mensagem) throws IOException {
+        //List<String[]> linhas = new ArrayList<>();
+        //linhas.add(new String[]{dataHoraLog, casoDeTeste, mensagem});
+
+        String[] linha = new String[]{dataHoraLog, casoDeTeste, mensagem};
+
+        //Writer escritor = Files.newBufferedWriter(Paths.get("target/logs/log - " + dataHora + ".csv"));
+
+        FileWriter escritor = new FileWriter("target/logs/log - " + dataHora + ".csv", true);
+        CSVWriter escritorCSV = new CSVWriter(escritor);
+
+        escritorCSV.writeNext(linha);
+
+        escritorCSV.flush();
+        escritor.close();
+    }
+
+    // Gravar cada linha no log
+    public void registrarCSV(String casoDeTeste, String mensagem) throws IOException {
+        String dataHoraLog = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS").format(Calendar.getInstance().getTime());
+
+        String[] linha = new String[]{dataHoraLog, casoDeTeste, mensagem};
+
+        FileWriter escritor = new FileWriter("target/logs/log - " + dataHora + ".csv", true);
+        CSVWriter escritorCSV = new CSVWriter(escritor);
+
+        escritorCSV.writeNext(linha);
+
+        escritorCSV.flush();
+        escritor.close();
+    }
+}
--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -1,100 +1,129 @@
-// 1-Pacotes
+// 1 - Pacote
 package webTests;
 
-// 2-Bibliotecas
+// 2 - Bibliotecas
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.Evidencias;
+import utils.Logs;
 
 import java.io.IOException;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
-import utils.Evidencias;
 
-// 3-Classe
-public class seleniumSimples<dependsOnMethods> {
-    // 3.1-Atributos
+// 3 - Classe
+public class seleniumSimples {
+    // 3.1 - Atributos
 
-    WebDriver driver;                       // declarar o objeto do Selenium WebDriver
+    WebDriver driver;                    //Declarar o objeto do Selenium WebDriver
     Evidencias evidencias;
-    static String dataHora = new SimpleDateFormat("YYYY-MM-dd HH-mm-ss").getCalendar().getTime().toString();
+    Logs logs;
+    static String dataHora = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
 
-    // 3.2-Métodos e Funções
-    @BeforeMethod
+    // 3.2 - Métodos e Funções
+
+
+    @BeforeClass                        //Executa 1 vez apenas, no inicio da execução do script
+    public void antesDeTudo() throws IOException {
+        logs = new Logs();
+        logs.iniciarLogCSV(dataHora);
+    }
+
+    @BeforeMethod                       //Executa antes de cada @Test
     public void iniciar(){
         // A - Início
-        System.setProperty("webdriver.chrome.driver", "drivers/chrome/90/chromedriver");
+
+        System.setProperty("webdriver.chrome.driver", "drivers/chrome/90/chromedriver");       // Aponta para onde está o driver do Chrome
+        // Instancia o objeto driver como um controlador do Chrome
         driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1600, 833));
         driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
 
-        evidencias = new Evidencias();
+        evidencias = new Evidencias(); // instanciar
+
     }
 
     @AfterMethod
     public void finalizar(){
-        // C - Finalizar o teste
-        driver.quit();
-    }
+        // Parte C - Encerramento
 
-    @Test(priority = 2, dependsOnMethods = {"consultarCursoMantis"})
-       public void consultarCursoCTFL() throws IOException {
-        String casoDeTeste = "Consultar Curso CTFL";
+        driver.quit(); // Encerra o objeto do Selenium
 
-        // B - Realziar o teste
-        driver.get("https://www.iterasys.com.br");                                          // abre o site
-        evidencias.print(driver, dataHora, casoDeTeste,"Passo 1 - Abriu o site");
-
-        driver.findElement(By.id("searchtext")).click();                                    // clica no campo de pesquisa
-        driver.findElement(By.id("searchtext")).clear();                                    // limpa o campo de pesquisa
-        driver.findElement(By.id("searchtext")).sendKeys("preparatório ctfl");  // escreve no campo de pesquisa
-
-        evidencias.print(driver, dataHora, casoDeTeste,"Passo 2 - Digitou Preparatorio CTFL");
-        // Clique na lupa
-        driver.findElement(By.id("btn_form_search")).click();
-
-        assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"preparatório ctfl\"");
-
-        evidencias.print(driver, dataHora, casoDeTeste,"Passo 3 - Exibiu a lista de cursos");
-        driver.findElement(By.cssSelector("span.comprar")).click();
-
-        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Preparatório CTFL");
-        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 169,00");
-        evidencias.print(driver, dataHora, casoDeTeste,"Passo 4 - Exibiu o carrinho de compras");
     }
 
     @Test(priority = 1)
     public void consultarCursoMantis() throws IOException {
         String casoDeTeste = "Consultar Curso Mantis";
-
         // B - Realizar o teste
-        driver.get("https://www.iterasys.com.br");                                          // abre o site
+        logs.registrarCSV(casoDeTeste, "Iniciou o Teste");
+        driver.get("https://www.iterasys.com.br");                              // Abre o site alvo informado
+        logs.registrarCSV(casoDeTeste, "Abriu o Site");
         evidencias.print(driver, dataHora, casoDeTeste, "Passo 1 - Abriu o site");
 
-        driver.findElement(By.id("searchtext")).click();                                    // clica no campo de pesquisa
-        driver.findElement(By.id("searchtext")).clear();                                    // limpa o campo de pesquisa
-        driver.findElement(By.id("searchtext")).sendKeys("mantis");             // escreve no campo de pesquisa
+        driver.findElement(By.id("searchtext")).click();                        // Clica no campo de pesquisa
+        logs.registrarCSV(casoDeTeste, "Clicou na caixa de pesquisa");
+        driver.findElement(By.id("searchtext")).clear();                        // Limpa o campo de pesquisa
+        logs.registrarCSV(casoDeTeste, "Limpou a caixa de pesquisa");
+        driver.findElement(By.id("searchtext")).sendKeys("mantis");  // Escreve "mantis" no campo
+        logs.registrarCSV(casoDeTeste, "Escreveu Mantis na caixa de pesquisa");
 
         evidencias.print(driver, dataHora, casoDeTeste,"Passo 2 - Digitou Mantis");
-        // Clique na lupa
-        driver.findElement(By.id("btn_form_search")).click();
+        driver.findElement(By.id("btn_form_search")).click();                   // Clique na lupa
+        logs.registrarCSV(casoDeTeste, "Clicou no botão lupa");
 
         assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"mantis\"");
+        logs.registrarCSV(casoDeTeste, "Confirmou o texto indicativo na pesquisa do curso Mantis");
+        //assertTrue(driver.findElement(By.cssSelector("h3")).getText().contains("mantis"));
+        //assertEquals(driver.findElement(By.cssSelector("h3:nth-child(1)")).getText(), "Cursos › \"mantis\"");
 
         evidencias.print(driver, dataHora, casoDeTeste,"Passo 3 - Exibiu a lista de cursos");
-        driver.findElement(By.cssSelector("span.comprar")).click();
+        logs.registrarCSV(casoDeTeste, "Clicou no botão Matricule-se");
+        driver.findElement(By.cssSelector("span.comprar")).click();             // Clica no botão Matricule-se
 
         assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Mantis");
+        logs.registrarCSV(casoDeTeste, "Confirmou o nome do curso Mantis");
+
         assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 49,99");
+        logs.registrarCSV(casoDeTeste, "Confirmou o valor do curso Mantis");
         evidencias.print(driver, dataHora, casoDeTeste,"Passo 4 - Exibiu o carrinho de compras");
+        logs.registrarCSV(casoDeTeste, "Concluiu o teste");
     }
 
+    @Test(priority = 2, dependsOnMethods = {"consultarCursoMantis"})
+    public void consultarCursoCTFL() throws IOException {
+        String casoDeTeste = "Consultar Curso CTFL";
+
+        // B - Realizar o teste
+        driver.get("https://www.iterasys.com.br");                              // Abre o site alvo informado
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 1 - Abriu o site");
+
+        driver.findElement(By.id("searchtext")).click();                        // Clica no campo de pesquisa
+        driver.findElement(By.id("searchtext")).clear();                        // Limpa o campo de pesquisa
+        driver.findElement(By.id("searchtext")).sendKeys("preparatório ctfl");  // Escreve "mantis" no campo
+
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 2 - Digitou preparatorio ctfl");
+        driver.findElement(By.id("btn_form_search")).click();                   // Clique na lupa
+
+        assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"preparatório ctfl\"");
+        //assertTrue(driver.findElement(By.cssSelector("h3")).getText().contains("mantis"));
+        //assertEquals(driver.findElement(By.cssSelector("h3:nth-child(1)")).getText(), "Cursos › \"mantis\"");
+
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 3 - Exibiu a lista de cursos");
+        driver.findElement(By.cssSelector("span.comprar")).click();             // Clica no botão Matricule-se
+
+        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText().toUpperCase(), "PREPARATÓRIO CTFL");
+        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 169,00");
+        evidencias.print(driver, dataHora, casoDeTeste,"Passo 4 - Exibiu o carrinho de compras");
+
+    }
 }
