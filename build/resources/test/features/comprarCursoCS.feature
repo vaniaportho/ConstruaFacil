f7534131de9ecff999b2b71b6db71febc0d18d73
Vania Porto <vaniaportho@gmail.com>
2021-07-28 20:08:24 -0300

    Cadastro, consulta e login de um usuário

--- build/resources/test/features/comprarCursoCS.feature
+++ build/resources/test/features/comprarCursoCS.feature
@@ -1,6 +1,6 @@
 #language: pt
 Funcionalidade: : Comprar Curso CS
-  Cenario: Pesquisar Curso e Incluir no Carrinho
+  Cenario: Pesquisar Curso com Clique e Incluir no Carrinho
     Dado que acesso o site da Iterasys
     Quando pesquiso por "Mantis"
     E clico na lupa
@@ -8,4 +8,22 @@ Funcionalidade: : Comprar Curso CS
     Quando clico em Matricule-se
     Entao confirmo o nome do curso como "Mantis" e o preco do curso de "R$ 49,99"
 
+  Cenario: Pesquisar Curso com Enter e Incluir no Carrinho
+    Dado que acesso o site da Iterasys
+    Quando pesquiso por "Mantis"
+    E clico pressiono Enter
+    Entao vejo a lista de resultados para o curso "Mantis"
+    Quando clico em Matricule-se
+    Entao confirmo o nome do curso como "Mantis" e o preco do curso de "R$ 49,99"
 
+  Esquema do Cenario: Pesquisar Curso e Incluir no Carrinho
+    Dado que acesso o site da Iterasys
+    Quando pesquiso por <curso>
+    E clico pressiono Enter
+    Entao vejo a lista de resultados para o curso <curso>
+    Quando clico em Matricule-se
+    Entao confirmo o nome do curso como <curso> e o preco do curso de <preco>
+    Exemplos:
+      | curso               | preco       |
+      | "Mantis"            | "R$ 49,99"  |
+      | "Preparatório CTFL" | "R$ 169,00" |
--- src/test/java/webTests/comprarCursoCS.java
+++ src/test/java/webTests/comprarCursoCS.java
@@ -1,16 +1,16 @@
 package webTests;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.E;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,14 +20,19 @@ import static org.testng.AssertJUnit.assertTrue;
 public class comprarCursoCS {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @Before
     public void iniciar(){
-        System.setProperty("webdriver.chrome.driver", "drivers/chrome/90/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "drivers/chrome/92/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1600, 833));
         driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+
+        wait = new WebDriverWait(driver,60,1);
+
         System.out.println("0 - Antes do Teste iniciar");
+
     }
 
     @After
@@ -43,11 +48,11 @@ public class comprarCursoCS {
     }
 
     @Quando("^pesquiso por \"([^\"]*)\"$")
-    public void pesquisoPor(String Curso) {
+    public void pesquisoPor(String curso) {
         driver.findElement(By.id("searchtext")).click();
         driver.findElement(By.id("searchtext")).clear();
-        driver.findElement(By.id("searchtext")).sendKeys(Keys.chord("Mantis"));
-        System.out.println("2 - Digitou o nome do curso " + Curso);
+        driver.findElement(By.id("searchtext")).sendKeys(Keys.chord(curso));
+        System.out.println("2 - Digitou o nome do curso " + curso);
     }
 
     @E("^clico na lupa$")
@@ -57,9 +62,13 @@ public class comprarCursoCS {
     }
 
     @Entao("^vejo a lista de resultados para o curso \"([^\"]*)\"$")
-    public void vejoAListaDeResultadosParaOCurso(String Curso) {
-        assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"" + Curso + "\"");
-        System.out.println("4 - Exibiu a lista de resultados para o curso " + Curso);
+    public void vejoAListaDeResultadosParaOCurso(String curso) {
+        String textoEsperado = "Cursos › \"" + curso + "\"";
+        WebElement h3texto = driver.findElement(By.cssSelector("h3"));
+        wait.until(ExpectedConditions.textToBePresentInElement(h3texto, textoEsperado));
+
+        assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Cursos › \"" + curso + "\"");
+        System.out.println("4 - Exibiu a lista de resultados para o curso " + curso);
     }
 
     @Quando("^clico em Matricule-se$")
@@ -69,9 +78,16 @@ public class comprarCursoCS {
     }
 
     @Entao("^confirmo o nome do curso como \"([^\"]*)\" e o preco do curso de \"([^\"]*)\"$")
-    public void confirmoONomeDoCursoComoEOPrecoDoCursoDe(String Curso, String Preco) {
-        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), Curso);
-        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), Preco);
-        System.out.println("6 - Confirmou o nome como " + Curso + " e o preco do curso como " + Preco);
+    public void confirmoONomeDoCursoComoEOPrecoDoCursoDe(String curso, String preco) {
+        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), curso);
+        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), preco);
+        System.out.println("6 - Confirmou o nome como " + curso + " e o preco do curso como " + preco);
     }
+
+    @E("^clico pressiono Enter$")
+    public void clicoPressionoEnter() {
+        driver.findElement(By.id("searchtext")).sendKeys(Keys.ENTER);
+        System.out.println("3a - Pressionou Enter");
+    }
+
 }
